370f36ecd08ffd5f338d0fc052c765c6e1db303d
Zainan Ali <zainanzaher09@gmail.com>
2024-10-03 16:48:22 +0100

    Axios installation for fetching data

--- fv-front/src/components/homepage/homepage.tsx
+++ fv-front/src/components/homepage/homepage.tsx
@@ -1,17 +1,32 @@
-// import Card from "../childcomps/card/card"
+import Card from "../childcomps/card/card"
 import { useSelector } from "react-redux"
 import { RootState } from "../store/store"
+import { useCallback, useEffect } from "react"
+import axios from '../../axiosInstance';
 export default function Home() {
+
+  const FetchPosts = useCallback( () => {
+    const response = axios.get("/posts")
+  }, [])
+
+  useEffect( () => {
+    FetchPosts()
+  }, [FetchPosts])
   const auth = useSelector((state: RootState) => state.auth)
   console.log(auth);
   return (
     <div className="container">
       {
-      <form action="">
+        auth.isLoggedIn && (
+
+          <form action="">
         <input type="text" />
         <input type="submit" name="" id="" />
       </form>
+      )
       }
+      {/* We will display all the psots here */}
+
     </div>
   )
 }
