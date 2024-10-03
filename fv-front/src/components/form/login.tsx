b0730d4a5c8bf7422ed06f051585bc2c68059f2a
Zainan Ali <zainanzaher09@gmail.com>
2024-10-03 15:54:01 +0100

    Created Form Base

---
+++ fv-front/src/components/form/login.tsx
@@ -0,0 +1,10 @@
+export default function Signup() {
+  return (
+    <form action="
+    ">
+      <input type="text" placeholder="email or username" id="" />
+      <input type="text" placeholder="password" id="" />
+      <input type="submit" value="login"/>
+    </form>
+  )
+}
---
+++ fv-front/src/components/form/signup.tsx
@@ -0,0 +1,11 @@
+export default function Signup() {
+  return (
+    <form action="
+    ">
+      <input type="text" placeholder="username"/>
+      <input type="text" placeholder="email" id="" />
+      <input type="text" placeholder="password" id="" />
+      <input type="submit" value="signup"/>
+    </form>
+  )
+}
