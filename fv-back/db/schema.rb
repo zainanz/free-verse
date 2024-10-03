e928c3c9300cfa2468b6f07f36bafbb91077b150
Zainan Ali <zainanzaher09@gmail.com>
2024-10-03 15:49:51 +0100

    Generated new Posts Model

--- fv-back/db/schema.rb
+++ fv-back/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_03_125426) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_03_143912) do
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id", null: false
+    t.integer "likes", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +34,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_03_125426) do
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
